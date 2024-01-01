d252b4a28a2a400c5425caa0d6bf725a4e7d70c0
NgThePhong0608 <nguyenthephong_t65@hus.edu.vn>
2024-01-01 12:13:56 +0700

    Refactor DefaultLayout and ContextProvider components

--- react/src/contexts/ContextProvider.jsx
+++ react/src/contexts/ContextProvider.jsx
@@ -7,31 +7,31 @@ const StateContext = createContext({
     setToken: () => {},
 });
 
-export const ContextProvider = ({children}) => {
-    const [user, setUser] = useState({
-        name: 'PhongNT'
-    });
-    const [token, _setToken] = useState(localStorage.getItem('ACCESS_TOKEN'));
-    // const [token, _setToken] = useState(123);
+// eslint-disable-next-line react/prop-types
+export const ContextProvider = ({ children }) => {
+    const [user, setUser] = useState({});
+    const [token, _setToken] = useState(localStorage.getItem("ACCESS_TOKEN"));
 
     const setToken = (token) => {
         _setToken(token);
         if (token) {
-            localStorage.setItem('ACCESS_TOKEN', token);
-        }else {
-            localStorage.removeItem('ACCESS_TOKEN');
+            localStorage.setItem("ACCESS_TOKEN", token);
+        } else {
+            localStorage.removeItem("ACCESS_TOKEN");
         }
-    }
+    };
     return (
-        <StateContext.Provider value={{
-            user, 
-            token, 
-            setUser, 
-            setToken
-        }}>
+        <StateContext.Provider
+            value={{
+                user,
+                token,
+                setUser,
+                setToken,
+            }}
+        >
             {children}
         </StateContext.Provider>
-    )
-}
+    );
+};
 
 export const useStateContext = () => useContext(StateContext);
